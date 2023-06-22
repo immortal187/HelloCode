9723fb08b085ac3bf4651bad187524c75699c72b
3eLeHblu <innocent87@mail.ru>
2023-06-23 00:20:57 +0300

    version 0.1.01

--- Example005_IfElse/Program.cs
+++ Example005_IfElse/Program.cs
@@ -2,7 +2,7 @@
 Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
 
-if(username == "Надя")
+if(username == "25")
 
 {
     Console.WriteLine ("Ура, Моя Надюшка пришла!!!");
