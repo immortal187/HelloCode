39f566e1d380f4a2a9bed3924d494abce8c90809
3eLeHblu <innocent87@mail.ru>
2023-06-25 12:45:37 +0300

    Лекция 2

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿int [] array = {2,4,5,543,5,6,10,1};
+
+int n = array.Length;
+int index = 0;
+
+int find = 5;
+
+
+
+while (index < n) 
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;    
+}
