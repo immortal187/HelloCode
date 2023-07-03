123766f26c6874018961105be71dd936ce28ed4c
3eLeHblu <innocent87@mail.ru>
2023-07-03 17:45:54 +0300

    лекция3 full

---
+++ Example012_SortMassive/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] arr = {1,5,3,6,7,8,9,6,4,5,6,8,1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write(array[i] + " ");   
+    }
+
+    Console.WriteLine();    
+}
+
+void SelectionSort(int[] array)
+{
+
+    for (int i = 0; i < array.Length -1; i++)    
+    {
+        int minPosition = i;
+
+        for (int j = i+1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+
+
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+
+    }
+
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
